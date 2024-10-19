3f3fc72104d21d3e5b4be72b2d25c40503f14fd3
shntykrn <shntykrn@gmail.com>
2024-10-19 22:21:44 +0700

    uploaded carsModel

---
+++ services/carsModel.js
@@ -0,0 +1,54 @@
+const modelsRepository = require("../repositories/models");
+const {
+    NotFoundError,
+    InternalServerError,
+} = require("../utils/request");
+
+exports.getModelById = async (id) => {
+    const model = await modelsRepository.getModelById(id);
+    if (!model) {
+        throw new NotFoundError("Model is Not Found!");
+    }
+    return model;
+};
+
+exports.createModel = async (data) => {
+    try {
+        return await modelsRepository.createModel(data);
+    } catch (error) {
+        throw new InternalServerError("Failed to create Model!");
+    }
+};
+
+exports.updateModel = async (id, data) => {
+    const existingModel = await modelsRepository.getModelById(id);
+    if (!existingModel) {
+        throw new NotFoundError("Model is Not Found!");
+    }
+
+    data = {
+        ...existingModel,
+        ...data,
+    };
+
+    const updatedModel = await modelsRepository.updateModel(id, data);
+    if (!updatedModel) {
+        throw new InternalServerError("Failed to update Model!");
+    }
+
+    return updatedModel;
+};
+
+exports.deleteModelById = async (id) => {
+    const existingModel = await modelsRepository.getModelById(id);
+    if (!existingModel) {
+        throw new NotFoundError("Model is Not Found!");
+    }
+
+    const deletedModel = await modelsRepository.deleteModelById(id);
+    if (!deletedModel) {
+        throw new InternalServerError("Failed to delete Model!");
+    }
+
+    return deletedModel;
+};
